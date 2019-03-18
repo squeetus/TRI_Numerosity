ed7f745580051bf57ee3a818c30c53c71ce68de5
squeetus <burlinsond2@gmail.com>
2019-03-18 09:37:16 -0400

    Store facilities and chemical usage in db

--- readFiles.js
+++ readFiles.js
@@ -1,3 +1,5 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 const csv = require('csv-parser')
 const fs = require('fs')
 const facilities = {};
@@ -40,7 +42,7 @@ class usage {
   constructor(row) {
     this.id = row.TRI_FACILITY_ID; // foreign key to facility metadata
     this.year = row.YEAR;
-    this.numChemicals = 1;
+    this.chemical = row.CHEMICAL;
 
     this.total_released = +row.TOTAL_RELEASES;
 
@@ -50,23 +52,9 @@ class usage {
 
     this.total_treated = +row['8.7_TREATMENT_OFF-SITE'] + (+row['8.6_TREATMENT_ON-SITE']);
   }
-
-  // add successive chemical usage
-  addRow(row) {
-    this.numChemicals++;
-
-    this.total_released += +row.TOTAL_RELEASES;
-
-    this.total_recycled += +row['OFF-SITE_RECYCLED_TOTAL'] + (+row['8.4_RECYCLING_ON-SITE']);
-
-    this.total_recovered += +row['8.3_ENERGY_RECOVERY_OFF-SITE'] + (+row['8.2_ENERGY_RECOVERY_ON-SITE']);
-
-    this.total_treated += +row['8.7_TREATMENT_OFF-SITE'] + (+row['8.6_TREATMENT_ON-SITE']);
-  }
-
 }
 
-readDataFile = function(year) {
+readDataFile = function(year, cb) {
   fs.createReadStream('./../datafiles/TRI_'+year+'_US.csv')
     .pipe(csv())
     .on('data', (data) => {
@@ -86,8 +74,74 @@ readDataFile = function(year) {
       console.log(Object.keys(facilities).length + ' unique facilities seen; ', usages.length + ' chemical uses logged');
 
       if(year > 2000)
-        readDataFile(--year);
+        readDataFile(--year, cb);
+      else {
+        cb();
+      }
     });
 };
 
-readDataFile(2017);
+
+/*
+  Set up DB connection
+*/
+(async function() {
+  // Connection URL
+  const url = 'mongodb://localhost:27017/TRI_Parser';
+  // Database Name
+  const dbName = 'TRI_Parser';
+  const client = new MongoClient(url);
+
+  try {
+    // Use connect method to connect to the Server
+    await client.connect();
+
+    const db = client.db(dbName);
+
+    // read all datafiles, then save to db
+    readDataFile(2017, function() {
+      saveFacilitiesToDB(db, function() {
+        saveUsagesToDB(db, function() {
+          console.log('ok closing db connection');
+          client.close();
+        });
+      });
+    });
+
+  } catch (err) {
+    console.log('!', err.stack);
+  }
+})();
+
+const saveFacilitiesToDB = function(db, callback) {
+  // Save the facilities (metadata) to the db
+  const collection = db.collection('facilities');
+
+  // clear old data
+  collection.remove();
+
+  collection.insertMany(Object.values(facilities), function(err, result) {
+    assert.equal(err, null);
+    let numFac = Object.keys(facilities).length;
+    assert.equal(numFac, result.result.n);
+    assert.equal(numFac, result.ops.length);
+    console.log("Inserted", numFac, "facilities");
+    callback(result);
+  });
+};
+
+const saveUsagesToDB = function(db, callback) {
+  // Save the chemical usage to the db
+  const collection = db.collection('usage');
+
+  // clear old data
+  collection.remove();
+
+  collection.insertMany(usages, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(usages.length, result.result.n);
+    assert.equal(usages.length, result.ops.length);
+    console.log("Inserted", usages.length, "usages");
+    callback(result);
+  });
+};
